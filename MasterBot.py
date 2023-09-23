485c1543ac9e49e8caf6b07c03a9866d28571339
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2023-09-23 14:46:34 +0530

    MasterBot Description change

--- MasterBot.py
+++ MasterBot.py
@@ -1,4 +1,4 @@
-# Description --> This is a python program create a System Information file
+# Description --> This module create a System Information file
 # Developer --> Soumalya Mondal
 # Prerequisite --> datetime (Python-inbuild)
 # File Location --> ~/System-Info/MasterBot.py
