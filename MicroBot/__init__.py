e9d92c8b673bf28563b973e10536c60900591048
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2023-09-23 14:43:30 +0530

    23-Sep-2023 MicroBot folder create with initialization

---
+++ MicroBot/__init__.py
@@ -0,0 +1,4 @@
+# Description --> This module create initialization for the all MicroBot
+# Developer --> Soumalya Mondal
+# Prerequisite --> 
+# File Location --> ~/System-Info/MicroBot/__init__.py
