f951a3d9d234a99a345d245fa02b2b2e1aaa8c84
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2023-09-24 16:02:55 +0530

    Functionality add, About section update

--- MasterBot.py
+++ MasterBot.py
@@ -1,19 +1,28 @@
+# ---------------- About ---------------- #
+# Name --> MasterBot.py
 # Description --> This module create a System Information file
 # Developer --> Soumalya Mondal
 # Prerequisite --> TimeInfo_MiB (Dev-Build)
 # File Location --> ~/System-Info/MasterBot.py
 # Callable Function -->
+# ------------------------------------------------ #
 
 # ---------------- Impoting Module ---------------- #
 # Importing TimeInfo_MiB module
 from MicroBot.TimeInfo_MiB import TimeInfo
+# Importing NameGenerator_MiB module
+from MicroBot.NameGenerator_MiB import NameGenerator
 # ------------------------------------------------ #
 
 # ---------------- Initial Main Function ---------------- #
 if __name__ == '__main__':
     # Create TimeInfo objet
     timeinfo = TimeInfo()
+    # Create NameGenerator object
+    namegenerator = NameGenerator()
 
     # Print TimeInfo object
-    print(f'Date:{timeinfo.date()} Time(12h):{timeinfo.time12h()} Time(24h):{timeinfo.time24h()}')
+    print(f'\nFrom TimeInfo Module\nDate:{timeinfo.date()}\nTime(12h):{timeinfo.time12h()}\nTime(24h):{timeinfo.time24h()}')
+    # Print NameGenerator object
+    print(f'\nFrom NameGenerator Module\nFolder Name:{namegenerator.foldername()}\nFile Name:{namegenerator.filename()}')
 # ------------------------------------------------ #
