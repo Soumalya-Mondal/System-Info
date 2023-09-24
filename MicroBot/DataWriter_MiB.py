60607e3837cef2dd511d49cdfb12a6c9e7e126ad
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2023-09-24 14:26:52 +0530

    Add DataWiter to microbot

---
+++ MicroBot/DataWriter_MiB.py
@@ -0,0 +1,12 @@
+# ---------------- About ---------------- #
+# Name --> DataWriter_MiB.py
+# Description --> This module write data into file
+# Developer --> Soumalya Mondal
+# Prerequisite --> 
+# File Location --> ~/System-Info/MicroBot/DataWriter_MiB.py
+# Callable Function --> 
+# ------------------------------------------------ #
+
+# ---------------- Impoting Module ---------------- #
+
+# ------------------------------------------------ #
