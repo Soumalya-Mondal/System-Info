5ff320ed6e2b0efd4c54be374bb1caa0aaf91136
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2023-09-25 23:47:49 +0530

    TimeInfo Funtionality Update

--- MicroBot/TimeInfo_MiB.py
+++ MicroBot/TimeInfo_MiB.py
@@ -1,55 +1,75 @@
 # ---------------- About ---------------- #
 # Name --> TimeInfo_MiB.py
-# Description --> This module create various time information
+# Description --> This module create various date & time information
 # Developer --> Soumalya Mondal
 # Prerequisite --> datetime (Python-inBuild)
 # File Location --> ~/System-Info/MicroBot/TimeInfo_MiB.py
 # Callable Function --> date(), time12h(), time24h()
 # ------------------------------------------------ #
 
-# ---------------- Impoting Module ---------------- #
-# Importing datetime module
-from datetime import datetime as dt
-# ------------------------------------------------ #
 
-# ---------------- Define Module Class ---------------- #
-class TimeInfo:
 
-    # Initilize the class
-    def __init__(self):
-        self.dtNow = dt.now()
-
-    # Create today's date fucntion
-    def date(self):
-        date = self.dtNow.strftime('%d-%b-%Y')
-        return date
-    
-    # Create current time(12H) function
-    def time12h(self):
-        time = self.dtNow.strftime('%I:%M:%S.%f %p')
-        return time
-    
-    # Create current time(24H) function
-    def time24h(self):
-        time = self.dtNow.strftime('%H:%M:%S.%f')
-        return time
-# ------------------------------------------------ #
-
-# ---------------- Debug Section ---------------- #
+# ---------------- Self Module Call Section ---------------- #
 # Calling main function
 if __name__ == '__main__':
+    # ---------------- Impoting Module ---------------- #
+    from datetime import datetime as dt
+
+    # ---------------- Define Class ---------------- #
+    class TimeInfo:
+        # Initilize the class
+        def __init__(self):
+            self.dtNow = dt.now()
+
+        # Create date fucntion
+        def date(self):
+            date = self.dtNow.strftime('%d-%b-%Y')
+            return date
+        
+        # Create current time(12H) function
+        def time12h(self):
+            time = self.dtNow.strftime('%I:%M:%S.%f %p')
+            return time
+        
+        # Create current time(24H) function
+        def time24h(self):
+            time = self.dtNow.strftime('%H:%M:%S.%f')
+            return time
 
     # Define TimeInfo class object
     debug = TimeInfo()
+    print('\n# ' + '-'*8 + ' System Current Date & Time Information ' + '-'*8 + ' #\n')
+    print(f'Curret Date: {debug.date()}')
+    print(f'Current Time(12H): {debug.time12h()}')
+    print(f'Current Time(24H): {debug.time24h()}')
+    print('\n# ' + '-'*48 + ' #\n')
+# ---------------------------------------------------------------- #
 
-    # Printing all function output to check
-    print('# ' + '-'*8 + ' System Current Date & Time Information ' + '-'*8 + ' #')
-    # Print today's date
-    print(f'\tCurret Date: {debug.date()}')
-    # Print current time(12H)
-    print(f'\tCurrent Time(12H): {debug.time12h()}')
-    # Print current time(24H)
-    print(f'\tCurrent Time(24H): {debug.time24h()}')
-    # End line print
-    print('# ' + '-'*48 + ' #')
-# ------------------------------------------------ #
+
+
+# ---------------- Other Module Call Section ---------------- #
+else:
+    # ---------------- Impoting Module ---------------- #
+    from datetime import datetime as dt
+
+    # ---------------- Define Class ---------------- #
+    class TimeInfo:
+        # Initilize the class
+        def __init__(self):
+            self.dtNow = dt.now()
+
+        # Create date fucntion
+        def date(self):
+            date = self.dtNow.strftime('%d-%b-%Y')
+            return date
+        
+        # Create current time(12H) function
+        def time12h(self):
+            time = self.dtNow.strftime('%I:%M:%S.%f %p')
+            return time
+        
+        # Create current time(24H) function
+        def time24h(self):
+            time = self.dtNow.strftime('%H:%M:%S.%f')
+            return time
+# ---------------------------------------------------------------- #
