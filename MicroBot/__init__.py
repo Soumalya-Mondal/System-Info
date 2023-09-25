5b44827de76e4842eda716818c9e48c31f156a44
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2023-09-25 23:43:45 +0530

    Funtionality Update

--- MicroBot/__init__.py
+++ MicroBot/__init__.py
@@ -1,9 +1,11 @@
+# ---------------- About ---------------- #
+# Name --> __init__.py
 # Description --> This module create initialization for the all MicroBot
 # Developer --> Soumalya Mondal
-# Prerequisite --> 
+# Prerequisite --> TimeInfo_MiB (Dev-Build), DataWriter_MiB (Dev-Build), NameGenerator_MiB (Dev-Build)
 # File Location --> ~/System-Info/MicroBot/__init__.py
 # Callable Function -->
+# ------------------------------------------------ #
 
 # ------------ Create Allocation For User Define Modules ---------------- #
 __all__ = ['TimeInfo_MiB', 'DataWriter_MiB', 'NameGenerator_MiB']
-# ------------------------------------------------ #
