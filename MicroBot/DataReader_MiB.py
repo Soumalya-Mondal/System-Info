6afcdffd455e0f29c428dc2e4d77092f462775c4
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2023-10-06 09:16:55 +0530

    DataReader skeleton add

---
+++ MicroBot/DataReader_MiB.py
@@ -0,0 +1,24 @@
+# ---------------- About ---------------- #
+# Name --> DataReader_MiB.py
+# Description --> This module read varity of system information
+# Developer --> Soumalya Mondal
+# Prerequisite --> subprocess (Python-inBuild)
+# File Location --> ~/System-Info/MicroBot/DataReader_MiB.py
+# Callable Function --> 
+# ---------------------------------------------------------------- #
+
+
+
+# ---------------- Self Module Call Section ---------------- #
+if __name__ == '__main__':
+    # ---------------- Importing Module ---------------- #
+    from subprocess import run
+
+    # ---------------- Define Property ---------------- #
+    
+
+    # ---------------- Define Class ---------------- #
+    class DataReader:
+        # Initialize the class
+        def __init__(self):
+            pass
